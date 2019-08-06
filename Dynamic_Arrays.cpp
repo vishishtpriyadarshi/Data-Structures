7e0b9ba03eab8636931f7e2234f75d19a81bb42f
Vishisht Priyadarshi <45481025+vishishtpriyadarshi@users.noreply.github.com>
2019-08-06 16:07:25 +0530

    Update Dynamic_Arrays.cpp

--- Dynamic_Arrays.cpp
+++ Dynamic_Arrays.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int* addArray(int n,int *A,int* ct,int* size)
 {
 
-    if(*ct < *size)
+    if(*ct < *size)                     // Insertion in O(1) time
     {
         A[*ct] = n;
         (*ct)++;
@@ -16,7 +16,7 @@ int* addArray(int n,int *A,int* ct,int* size)
 
     else
     {
-        int *A2 = new int[(*ct)*2];
+        int *A2 = new int[(*ct)*2];      // Insertion in O(n) time
         (*size) = (*ct)*2;
 
         for(int i = 0; i<*ct; i++)
@@ -40,8 +40,8 @@ int main()
     int *A = new int[1];
     cout << "Enter elements:\n";
 
-    int size = 1;
-    int ct = 0;
+    int size = 1;     // SIZE of the Array
+    int ct = 0;       // Position of last inserted element
 
     while(l--)
     {
